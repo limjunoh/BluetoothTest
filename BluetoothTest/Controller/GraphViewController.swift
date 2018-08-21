3ddd3f1350d4d16d4aafe3825088a66a15f8f522
KiWonJang <jkwonny93@ajou.ac.kr>
2018-08-21 15:36:39 +0900

    added graph which need to be modified

--- BluetoothTest/Controller/GraphViewController.swift
+++ BluetoothTest/Controller/GraphViewController.swift
@@ -17,6 +17,7 @@ class GraphViewController: UIViewController {
     @IBOutlet weak var disconnectButton: UIButton!
     @IBOutlet weak var deviceName: UITextField!
     
+    //line chart의 y value array
     var Yvalue = (0..<100).map{(i)-> Double in
         return Double(0)
     }
@@ -38,16 +39,21 @@ class GraphViewController: UIViewController {
         disconnectButton.isEnabled = false
         
         NotificationCenter.default.addObserver(self, selector: #selector(reloadView), name: NSNotification.Name(rawValue: "bluetoothStatusChanged"), object: nil)
-        
+        //기본 chart 생성
         initChart()
-        // Do any additional setup after loading the view.
+        
     }
     
-    
+    override func viewDidAppear(_ animated: Bool) {
+        serial.delegate = self
+    }
+    deinit {
+        NotificationCenter.default.removeObserver(self)
+        // 옵저버 제거
+    }
     
     @objc func reloadView() {
         serial.delegate = self
-        
         if serial.isReady { // 연결 시
             let device = serial.connectedPeripheral?.name ?? "Unknown"
             deviceName.text = device
@@ -58,12 +64,13 @@ class GraphViewController: UIViewController {
     }
     
     func initChart(){
-        var lineChartEntry = [ChartDataEntry]()
+        var lineChartEntry = [ChartDataEntry]() //charts에서 쓰는 자료형
         
         for i in 0..<Yvalue.count{
-            let value = ChartDataEntry(x: Double(i), y: 0)
+            let value = ChartDataEntry(x: Double(i), y: 0) //entry x,y 설정
             lineChartEntry.append(value)
         }
+        
         let set = getChartSet(values: lineChartEntry, label: "Cel")
         let data = LineChartData()
         data.addDataSet(set)
@@ -72,6 +79,7 @@ class GraphViewController: UIViewController {
         
         lineChartView.chartDescription?.text = "Temperature"
     }
+    //chart 업데이트
     func updateChart(yData:Double) {
         var lineChartEntry = [ChartDataEntry]()
         
@@ -90,8 +98,9 @@ class GraphViewController: UIViewController {
         lineChartView.chartDescription?.text = "Temperature"
     }
     
+    //chart 셋팅
     func getChartSet(values:[ChartDataEntry], label:String) -> LineRadarChartDataSet {
-        let set = LineChartDataSet(values: values, label: "Number")
+        let set = LineChartDataSet(values: values, label: label)
         set.colors = [NSUIColor.blue]
         set.mode = .cubicBezier
         set.lineWidth = 1.0
@@ -107,10 +116,10 @@ class GraphViewController: UIViewController {
     }
     
     @IBAction func clearAction(_ sender: UIButton) {
-        serial.disconnect()
         Yvalue = (0..<100).map{(i)-> Double in
             return Double(0)
         }
+        updateChart(yData: 0.0)
     }
 
     @IBAction func disconnectAction(_ sender: UIButton) {
@@ -139,8 +148,19 @@ extension GraphViewController:BluetoothSerialDelegate{
         }
     }
     //--??
+    func serialDidDiscoverPeripheral(_ peripheral: CBPeripheral, RSSI: NSNumber?) {
+        //        for existing in peripherals {
+        //            if existing.peripheral.identifier == peripheral.identifier { return }
+        //        }
+        //
+        //        let theRSSI = RSSI?.floatValue ?? 0.0
+        //        peripherals.append((peripheral: peripheral, RSSI: theRSSI))
+        //        peripherals.sort{ $0.RSSI < $1.RSSI }
+        //        tableView.reloadData()
+    }
     
     func serialDidReceiveBytes(_ bytes: [UInt8]) {
+        print("LOG:serialDidReceiveBytes = \(bytes)")
         // UInt8 Bytes [],[],[],[],[] 5개 read됨.
         // 그 중 1~4번째 인덱스에 있는 정보가 온도정보이므로 부동소수점 방식으로 온도 캘리브레이션
         let rawData = Array(bytes[1...4].reversed())
@@ -149,10 +169,9 @@ extension GraphViewController:BluetoothSerialDelegate{
         data.getBytes(&tempData, length: 4)
         tempData = UInt32(bigEndian: tempData)
         let mantissa = tempData & 0x00FFFFFF
-        let exponent = -1   // 보류...
+        let exponent = -3  // 보류...
         let actualTemp = Double(mantissa) * pow(10, Double(exponent))
-        
-        Yvalue.removeFirst()
-        Yvalue.append(actualTemp)
+        print(mantissa, exponent)
+        updateChart(yData: actualTemp)
     }
 }
